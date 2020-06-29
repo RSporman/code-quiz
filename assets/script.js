a539790c7a04bc3cb25941c8342acf80aa58833e
Bobby Sporman <bobby.sporman@yahoo.com>
2020-06-29 19:47:30 -0400

    Updated timer

--- assets/script.js
+++ assets/script.js
@@ -47,6 +47,29 @@ var questionList = [
 
 ];
 
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 3,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+
 
 var questionTag = document.body.querySelector("#question");
 var answerTagA = document.body.querySelector("#answer-a");
